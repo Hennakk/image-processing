e08bf5cdcde461bad2b668469adc36d82f1e3f4a
Hyun A Kim <98154707+Hennakk@users.noreply.github.com>
2023-03-06 16:45:33 +0900

    Update sin.cpp

--- image coordinates/sin.cpp
+++ image coordinates/sin.cpp
@@ -1,18 +1,23 @@
     int i, j, k;
     double pi=3.14159265;
     double temp, x, y, xi, yi;
-    for(i=0; i<256;i++)
-        for(j=0; j<256;j++){
-            RESULT[i][j]=200;
-        }
-    for(k=1; k<255;k++){
+    double theta=45*pi/180.0; //θ=45일때
+    n=(int)125+100*cos(theta);
+    for(k=1; k<360;k++){
         x=(double)k;
-        xi=k;
-        yi=100*cos(2*pi*x/120);
-        i=125+(int)yi;  //중간 x축을 기준으로 그래프가 그려져야하므로 125를 더함
-        j=(int)k;
-        RESULT[i][j]=300;
-        RESULT[125][j]=150; //x축
-        RESULT[j][10]=150;  //y축
+        xi=125+100*cos(2*pi*x/360);
+        yi=125+100*sin(2*pi*x/360);
+        i=(int)xi;
+        j=(int)yi;
+        RESULT[i][j]=0;
+        RESULT[125][j]=300;  //x축
+        RESULT[i][125]=300;  //y축
+        RESULT[j][n]=0;
     }
+        for(r=0;r<100;r++){
+            xx=(int)125-r*sin(theta);
+            yy=(int)125+r*cos(theta);
+            RESULT[xx][yy]=300; //빗금 r
+	    RESULT[xx][n]=300;  // sinθ
+        }
     DrawResultImage();
